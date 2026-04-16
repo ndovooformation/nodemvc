cb8ba3258258602c564f4d609da6f13db23ae17f
ndovooformation <formation@ndovoo.com>
2026-04-16 11:38:29 +0300

    initier la vue Accueil et le controlleur Accueil

--- app.js
+++ app.js
@@ -2,4 +2,11 @@
  * Le fichier app.js est une application de type expressjs
  */
 
+// J'importe le package expressjs
 const express = require("express");
+
+// J'initie l'aplication expressjs
+const app = express();
+
+
+module.exports = app;
---
+++ controllers/accueilController.js
@@ -0,0 +1,8 @@
+/**
+ * Ce fichier est un contrôleur.
+ * Dans ce fichier, je crée les routes de la page 'accueil.ejs'
+ */
+
+const express = require("express");
+
+const router = express.Router();
